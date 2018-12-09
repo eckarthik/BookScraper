221b96f750f4464f5c1e5593df4e020a348ad1be
eckarthik <eckarthik39@gmail.com>
2018-12-09 16:14:40 +0530

    Modified the script

--- scraper.py
+++ scraper.py
@@ -1,42 +1,52 @@
 import re,requests,json,csv
 from bs4 import BeautifulSoup
-req = requests.get("https://www.indiabookstore.net/categories/computerScience")
-soup = BeautifulSoup(req.text,"html.parser")
 books_counter = 0
-books = []
-for book in soup.find_all("a",{'class':'bookPageLink'}):
-    book_link = book.get("href")
-    if str(book_link).startswith("/isbn"):
-        books_counter+=1
-        book = []
-        req2 = requests.get("https://www.indiabookstore.net"+book_link)
-        soup = BeautifulSoup(req2.text,"html.parser")
-        getImageAndTitle = soup.find("img",{'class':'bookMainImage'})
-        imageLink = getImageAndTitle.get('src')
-        bookTitle = getImageAndTitle.get('title')
-        getAuthor = soup.find("div",{'itemprop':'author'})
-        authorName = str(getAuthor.text).split(":")[1]
-        print authorName
-        getBookDescription = soup.find("span",{"itemprop":"description"})
-        try:
-            bookDescription = getBookDescription.text
-        except AttributeError:
-            bookDescription = ""
-        '''book[str("image")] = imageLink
-        book[str("title")] = bookTitle
-        book[str("author")] = authorName
-        book[str("description")] = bookDescription.encode('ascii','ignore')
-        book[str("category")] = "Computer Science"'''
-        book.append(imageLink)
-        book.append(bookTitle)
-        book.append(authorName)
-        book.append(bookDescription.encode('ascii','ignore'))
-        book.append("Computer Science")
-        book.append(str(book_link).replace("/isbn/",""))
-        books.append(book)
-print books
-ofile = open('ttest.csv', "wb")
-writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-for row in books:
-    writer.writerow(row)
+ofile = open('ttest.csv', "wb",)
+writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL,lineterminator='\n')
+for i in range(1,6):
+    books = []
+    req = requests.get("https://www.indiabookstore.net/categories/wordPowerAndGrammar?page="+str(i))
+    soup = BeautifulSoup(req.text,"html.parser")
+    for book in soup.find_all("a",{'class':'bookPageLink'}):
+        book_link = book.get("href")
+        if str(book_link).startswith("/isbn"):
+            books_counter+=1
+            book = []
+            req2 = requests.get("https://www.indiabookstore.net"+book_link)
+            soup = BeautifulSoup(req2.text,"html.parser")
+            getImageAndTitle = soup.find("img",{'class':'bookMainImage'})
+            imageLink = getImageAndTitle.get('src')
+            bookTitle = getImageAndTitle.get('title')
+            getAuthor = soup.find("div",{'itemprop':'author'})
+            try:
+                authorName = str(getAuthor.text).split(":")[1]
+            except Exception:
+                continue
+            print authorName
+            getBookDescription = soup.find("span",{"itemprop":"description"})
+            try:
+                bookDescription = getBookDescription.text
+            except AttributeError:
+                bookDescription = ""
+                continue
+            '''book[str("image")] = imageLink
+            book[str("title")] = bookTitle
+            book[str("author")] = authorName
+            book[str("description")] = bookDescription.encode('ascii','ignore')
+            book[str("category")] = "Computer Science"'''
+            book.append(imageLink)
+            book.append(bookTitle)
+            book.append(authorName)
+            book.append(bookDescription.encode('ascii','ignore'))
+            book.append("Grammar")
+            book.append(str(book_link).replace("/isbn/",""))
+            books.append(book)
+    print books
+
+
+    for row in books:
+        try:
+            writer.writerow(row)
+        except Exception:
+            continue
